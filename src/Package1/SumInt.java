0534a4889d53b9b6ba163366b145cd17a945ad9a
YashwanthSampath <yashwanthsampath8@gmail.com>
2023-08-08 22:48:30 +0530

    Second commit

---
+++ src/Package1/SumInt.java
@@ -0,0 +1,12 @@
+package Package1;
+
+public class SumInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 10, c;
+		c = a + b;
+		System.out.print(c);
+	}
+
+}
