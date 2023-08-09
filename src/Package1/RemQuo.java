76bf5fb6818ffa26349fc29cb5b465bf000253cb
YashwanthSampath <yashwanthsampath8@gmail.com>
2023-08-09 09:44:31 +0530

    9th Aug

---
+++ src/Package1/RemQuo.java
@@ -0,0 +1,12 @@
+package Package1;
+
+public class RemQuo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 2;
+		int q = a/b, r=a%b;
+		System.out.print("quotent " + q +" remainder " + r);
+	}
+
+}
---
+++ src/Package1/SwapTwoNum.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class SwapTwoNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 20;
+		System.out.println("Before "+ a + " " + b);
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After " + a + " " + b);
+	}
+
+}
