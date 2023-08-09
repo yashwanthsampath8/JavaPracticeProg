2cd4609526bacd7f70ab90ff3f152df9bedc72ce
YashwanthSampath <yashwanthsampath8@gmail.com>
2023-08-09 09:36:40 +0530

    9th Aug

---
+++ src/Package1/AscivValue.java
@@ -0,0 +1,12 @@
+package Package1;
+
+public class AscivValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// assign character to integer variable and print the variable
+		int a = 'A';
+		System.out.print(a);
+	}
+
+}
---
+++ src/Package1/FloatMultiply.java
@@ -0,0 +1,12 @@
+package Package1;
+
+public class FloatMultiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 10.5, b = 10.5, c;
+		c = a * b;
+		System.out.print(c);
+	}
+
+}
