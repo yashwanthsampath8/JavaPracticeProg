cbd48402ae2775b9c85c998d5e7a78b2e69cac51
YashwanthSampath <yashwanthsampath8@gmail.com>
2023-08-10 09:31:25 +0530

    2 prog

---
+++ src/Package1/EvenOdd.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 1;
+		if(num%2 == 0) {
+			System.out.print("number is even");
+		}else
+		{
+			System.out.print("number is odd");
+		}
+	}
+
+}
---
+++ src/Package1/VowelConsonent.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class VowelConsonent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c = 'b';
+		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+			System.out.print("char is vowel");
+		}else
+		{
+			System.out.print("char is consonant");
+		}
+	}
+
+}
