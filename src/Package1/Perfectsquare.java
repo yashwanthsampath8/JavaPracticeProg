ce857b3bd1f3fb94ee09c74ed975a791898aaba6
YashwanthSampath <yashwanthsampath8@gmail.com>
2023-08-28 21:05:58 +0530

    perfect squares

---
+++ src/Package1/Perfectsquare.java
@@ -0,0 +1,12 @@
+package Package1;
+
+public class Perfectsquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1; i*i<=100;i++) {
+			System.out.println(i*i);
+		}
+	}
+
+}
