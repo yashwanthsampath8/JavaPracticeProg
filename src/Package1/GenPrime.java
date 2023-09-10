ad9d06d77425bfb17cfd78e17250761607493906
YashwanthSampath <yashwanthsampath8@gmail.com>
2023-09-10 21:09:50 +0530

    10th sep

---
+++ src/Package1/GenPrime.java
@@ -0,0 +1,23 @@
+package Package1;
+
+public class GenPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Generating prime numbers till N
+		
+		int n = 10, count = 0;
+		for(int i = 2 ; i <=n; i++) {
+			for(int j = 2; j<i;j++) {
+				count = 0;
+				if(i%j==0) {
+					 count++;
+		             break;
+				}
+			}
+	        if(count==0)
+	            System.out.println(i);
+		}
+	}
+
+}
